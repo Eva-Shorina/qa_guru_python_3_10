f73349a7e0230214cb1a607f81cb0c309980d1c9
Eva-Shorina <eva.shorina@yandex.ru>
2023-01-07 15:02:19 +0300

    change 2 lines in test

--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -3,9 +3,8 @@ from demoqa.data.user_data import alla
 
 def test_submitting_form():
 
-    user = alla
     form = Form()
-    form.submit_form(user)
+    form.submit_form(alla)
 
-    form.validate_form(user)
+    form.validate_form(alla)
 
