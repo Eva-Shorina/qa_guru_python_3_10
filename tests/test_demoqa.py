debaf8edaccf37c94a70ba26a742fdbe8cee97b1
Eva-Shorina <eva.shorina@yandex.ru>
2023-01-12 16:20:17 +0300

    initial commit

--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -1,10 +1,20 @@
+from allure_commons.types import Severity
+
 from demoqa.model.pages.practise_form import Form
 from demoqa.data.user_data import alla
+import allure
 
 def test_submitting_form():
+    allure.dynamic.tag('web')
+    allure.dynamic.severity(Severity.NORMAL)
+    allure.dynamic.label('owner', 'eva.shorina')
+    allure.dynamic.feature('Проверка отправки формы')
 
     form = Form()
-    form.submit_form(alla)
 
-    form.validate_form(alla)
+    with allure.step('Заполняем данные формы'):
+        form.submit_form(alla)
+
+    with allure.step('Проверяем, что данные формы есть в результирующей таблице'):
+        form.validate_form(alla)
 
